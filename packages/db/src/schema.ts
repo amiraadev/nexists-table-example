78839495a5448c8379a16b06cd565a8c0b24a0de
amiraadev <allagui_fawzi@outlook.fr>
2024-10-09 19:15:47 +0100

    feat: add post status enum, post and postView schemas, and filter logic
    - Defined postStatusEnum with draft, published, and rchived statuses
    - Added posts and postViews tables with relevant fields and default values
    - Created schemas for searchPostParams, createPost, updatePost, createPostView, editPostView, and deletePostView
    - Implemented filter parameters and operators for post views

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,5 +1,6 @@
 import { sql } from "drizzle-orm";
 import {
+  integer,
   json,
   pgEnum,
   pgTable,
@@ -8,30 +9,122 @@ import {
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
-import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
 //////////////////////// POSTS ////////////////////////
 
-export const Post = pgTable("post", {
+export const postStatusEnum = pgEnum(`post_status`, [
+  "draft",
+  "published",
+  "archived",
+]);
+
+export const posts = pgTable("posts", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
-  title: varchar("name", { length: 256 }).notNull(),
-  content: text("content").notNull(),
+  title: varchar("title", { length: 256 }).notNull(),
+  status: postStatusEnum("status").notNull().default("draft"),
+  authorName: varchar("authorName", { length: 256 }).notNull(),
+  commentsNumber: integer("commentsNumber").notNull().default(0),
   createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updatedAt", {
-    mode: "date",
-    withTimezone: true,
-  }).$onUpdateFn(() => sql`now()`),
+  updatedAt: timestamp("updated_at")
+  .default(sql`current_timestamp`)
+  .$onUpdate(() => new Date()),
 });
 
-export const CreatePostSchema = createInsertSchema(Post, {
-  title: z.string().max(256),
-  content: z.string().max(256),
-}).omit({
-  id: true,
-  createdAt: true,
-  updatedAt: true,
+export type Post = typeof posts.$inferSelect;
+export type NewPost = typeof posts.$inferInsert;
+
+export const postViews = pgTable("postViews", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  name: text("name").notNull().unique(),
+  columns: text("columns").array(),
+  filterParams: json("filterParams").$type<PostFilterParams>(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .default(sql`current_timestamp`)
+    .$onUpdate(() => new Date()),
 });
+
+export type PostView = typeof postViews.$inferSelect;
+
+export const searchPostParamsSchema = z.object({
+  page: z.coerce.number().default(1),
+  per_page: z.coerce.number().default(10),
+  sort: z.string().optional(),
+  title: z.string().optional(),
+  status: z.string().optional(),
+  authorName: z.string().optional(),
+  commentsNumber: z.string().optional(),
+  from: z.string().optional(),
+  to: z.string().optional(),
+  operator: z.enum(["and", "or"]).optional(),
+  viewId: z.string().uuid().optional(),
+});
+
+export type SearchPostParams = z.infer<typeof searchPostParamsSchema>;
+
+export const getPostsSchema = searchPostParamsSchema;
+
+export type GetPostsSchema = z.infer<typeof getPostsSchema>;
+
+export const createPostSchema = z.object({
+  title: z.string(),
+  authorName: z.string(),
+  status: z.enum(posts.status.enumValues),
+  commentsNumber: z.number(),
+});
+
+export type CreatePostSchema = z.infer<typeof createPostSchema>;
+
+export const updatePostSchema = z.object({
+  title: z.string().optional(),
+  authorName: z.string().optional(),
+  status: z.enum(posts.status.enumValues).optional(),
+  commentsNumber: z.number().optional(),
+});
+
+export type UpdatePostSchema = z.infer<typeof updatePostSchema>;
+
+export const createPostViewSchema = z.object({
+  name: z.string().min(1),
+  columns: z.string().array().optional(),
+  filterParams: z
+    .object({
+      operator: z.enum(["and", "or"]).optional(),
+      sort: z.string().optional(),
+      filters: z
+        .object({
+          field: z.enum(["title", "authorName", "status", "commentsNumber"]),
+          value: z.string(),
+          isMulti: z.boolean().default(false),
+        })
+        .array()
+        .optional(),
+    })
+    .optional(),
+});
+
+export type CreatePostViewSchema = z.infer<typeof createPostViewSchema>;
+
+export const editPostViewSchema = createPostViewSchema.extend({
+  id: z.string().uuid(),
+});
+export type EditPostViewSchema = z.infer<typeof editViewSchema>;
+
+export const deletePostViewSchema = z.object({
+  id: z.string().uuid(),
+});
+
+export type DeletePostViewSchema = z.infer<typeof deletePostViewSchema>;
+
+export type PostFilterParams = NonNullable<
+  CreatePostViewSchema["filterParams"]
+>;
+export type PostOperator = PostFilterParams["operator"];
+export type PostSort = PostFilterParams["sort"];
+export type PostFilter = NonNullable<PostFilterParams["filters"]>[number];
+
+//////////////////////// USER ////////////////////////
 
 export const User = pgTable("user", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
