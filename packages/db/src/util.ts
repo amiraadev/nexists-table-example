b8679c68a72386053536bcbe5f932a2370f7af33
amiraadev <allagui_fawzi@outlook.fr>
2024-10-09 19:19:51 +0100

    feat:Implemented generateRandomPost to create random post data excluding id

--- packages/db/src/util.ts
+++ packages/db/src/util.ts
@@ -1,8 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { customAlphabet } from "nanoid";
 
-import type { Task } from "./schema";
-import { tasks } from "./schema";
+import type { Post, Task } from "./schema";
+import { posts, tasks } from "./schema";
 
 export function generateRandomTask(): Omit<Task, "id"> {
   return {
@@ -13,6 +13,17 @@ export function generateRandomTask(): Omit<Task, "id"> {
     status: faker.helpers.shuffle(tasks.status.enumValues)[0] ?? "todo",
     label: faker.helpers.shuffle(tasks.label.enumValues)[0] ?? "bug",
     priority: faker.helpers.shuffle(tasks.priority.enumValues)[0] ?? "low",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+}
+
+export function generateRandomPost(): Omit<Post, "id"> {
+  return {
+    title: faker.word.words({ count: { min: 3, max: 7 } }),
+    status: faker.helpers.shuffle(posts.status.enumValues)[0] ?? "draft",
+    authorName: faker.internet.userName(),
+    commentsNumber: faker.number.int({ min: 0, max: 9000 }), // Set max to PostgreSQL INTEGER limit
     createdAt: new Date(),
     updatedAt: new Date(),
   };
